7f3c41df8415eb9d2be923115cd5d5cadc37f262
Leticia Martinez Tubio <petviolet@gmail.com>
2021-03-07 23:36:46 +0000

    Fixed error with security protocol URL

--- app/src/main/java/com/example/movielist/Constant.java
+++ app/src/main/java/com/example/movielist/Constant.java
@@ -2,7 +2,7 @@ package com.example.movielist;
 
 public class Constant {
     // Constant for call web service for get movies list and other information
-    public static final String URL_BASE = "http://api.themoviedb.org/3/";
+    public static final String URL_BASE = "https://api.themoviedb.org/3/";
     // Constant with key for API.
     public static final String API_KEY = "3348f5eb7e8779b5390af8bb41e48461";
     // Constant for get images from movies.
